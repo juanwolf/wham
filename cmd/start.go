011e960a4b5c83e02d20479549400286d6ae9f66
Jean-Loup Adde <jean-loup.adde@juanwolf.fr>
2018-02-11 23:07:10 +0000

    :sparkles: Start command now creates the csv file

--- cmd/start.go
+++ cmd/start.go
@@ -13,12 +13,17 @@ type startError struct {
 	message string
 }
 
-func (e *startError) Error() {
-	fmt.Println("Error -", e.message)
-	os.Exit(1)
+func (e *startError) Error() string {
+	return fmt.Sprintf("Error - %s", e.message)
+}
+
+func (e *startError) Terminate() {
 }
 
 const tmpFile = "/tmp/wham.lock"
+
+// DBFilePrefix contains the prefix for the "db" files created
+const DBFilePrefix = "oncall"
 
 var startCmd = &cobra.Command{
 	Use:   "start",
@@ -27,7 +32,8 @@ var startCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		err := start()
 		if err != nil {
-			err.Error()
+			fmt.Println(err.Error())
+			os.Exit(1)
 		}
 	},
 }
@@ -36,10 +42,35 @@ func init() {
 	rootCmd.AddCommand(startCmd)
 }
 
-func start() *startError {
-	_, err := os.Stat(tmpFile)
-	if !os.IsNotExist(err) {
+// Create the CSV 'db' for the current month
+func createCSV() (*os.File, error) {
+	thisMonth := time.Now().Month()
+	thisYear := time.Now().Year()
+	dbFilePath := fmt.Sprintf("%s/%s_%d_%d.csv", WhamDirectory, DBFilePrefix, thisMonth, thisYear)
+	dbFile, err := os.Create(dbFilePath)
+	if err != nil {
+		return dbFile, &startError{err.Error()}
+	}
+	return dbFile, nil
+}
+
+func start() error {
+	_, err := os.Stat(WhamDirectory)
+	if err != nil {
+		return &startError{`Wham not initialized! Run instead:
+wham init`}
+	}
+
+	_, err = os.Stat(tmpFile)
+	if err == nil {
 		return &startError{"Wham already started!"}
+	}
+
+	err = nil
+
+	_, sErr := createCSV()
+	if sErr != nil {
+		return sErr
 	}
 
 	startTime := time.Now()
